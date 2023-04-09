4fb535060f8bbececb08c6c429fb4c2333253081
lucasscurtoo <lucascurtoo@gmail.com>
2023-04-09 18:01:33 -0300

    adding security for the token desencriptation, and adding expiration time

--- src/middlewares/validate-token.js
+++ src/middlewares/validate-token.js
@@ -1,16 +1,27 @@
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 
 const verifyToken = (req, res, next) => {
-  const token = req.header("auth-token");
+  const token = req.header("auth-token")
   if (!token)
-    return res.status(401).json({ error: true, message: "Acceso denegado" });
+    return res.status(401).json({ error: true, message: "Access denied" })
   try {
-    const verify = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verify;
-    next();
-  } catch (error) {
-    res.status(401).json({ error: true, message: "Token no es valido" });
-  }
-};
+    const verify = jwt.verify(token, process.env.TOKEN_SECRET, {
+      algorithms: ["HS256"],
+    })
 
-module.exports = verifyToken;
+    if (req.body.username !== verify.username) {
+      return res.status(401).json({ error: true, message: "Unauthorized user" })
+    }
+
+    if (Date.now() >= verify.exp * 1000) {
+      return res.status(401).json({ error: true, message: "Token has expired" })
+    }
+
+    req.user = verify
+    next()
+  } catch (error) {
+    res.status(401).json({ error: true, message: "Token is not valid" })
+  }
+}
+
+module.exports = verifyToken
